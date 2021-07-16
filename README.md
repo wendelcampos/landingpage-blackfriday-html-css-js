9bfd529dfd8901e590c1bf0e7af1a7e651242e14
Wendel <waguiartrader@gmail.com>
2021-07-16 14:20:38 -0300

    desafio hiring coders

---
+++ README.md
@@ -0,0 +1,4 @@
+*Projeto landing page  feito em html, css e javascript sobre o tema black friday, desafio hiring coders* 
+
+
+
---
+++ js/main.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData);
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>carregando...</p>`
+
+    content.innerHTML = carregando;
+})
