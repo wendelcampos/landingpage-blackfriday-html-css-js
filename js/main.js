ed31e6fd20bd6dd2e2af0304129d031ec5127eb5
Wendel <waguiartrader@gmail.com>
2021-07-16 15:19:34 -0300

    alterações realizadas no javascript para o localstorage e acrescimo do arquivo .gif

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
     let data = {
         nome,
         email,
+        telefone,
     }
     let convertData = JSON.stringify(data);
 
@@ -14,7 +16,17 @@ form.addEventListener('submit', (e) => {
 
     let content = document.getElementById('content')
 
-    let carregando = `<p>carregando...</p>`
+    let carregando = `<img src='../asserts/loader.gif'>`
+
+    let pronto = `<h2>Obrigado !!!</h2>`
 
     content.innerHTML = carregando;
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 2000)
+
+    setTimeout(() => {
+        location.reload();
+    }, 4000)
 })
